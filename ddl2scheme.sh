7d3a10e554dcf260db7cf3b56e97178ea027fed0
Molchanov Alexander <molchanov_aa@transset.ru>
2017-05-24 14:17:36 +0300

    Ignore some errors for the REFRESH_GROUPS objects in the ddl2scheme.sh

--- ddl2scheme.sh
+++ ddl2scheme.sh
@@ -384,6 +384,18 @@ sql_finding_errors_in_sqlplus_output ( )
 		fi
 	fi
 
+	if [ $type_name = "REFRESH_GROUPS" ] ; then
+		# We ignore errors:
+		#   * ORA-23403: refresh group "XXX"."YYY" already exists
+		#   * ORA-23410: materialized view "ZZZ"."BBB" is already in a refresh group
+		# because the MATERIALIZED_VIEWS objects can create REFRESH_GROUPS auto (by the "REFRESH FORCE ON DEMAND" option).
+		if [ -n "$ora_ignore_addons" ] ; then
+			ora_ignore_addons="${ora_ignore_addons}|23403:|23410:"
+		else
+			ora_ignore_addons="(?!23403:|23410:"
+		fi
+	fi
+
 	if [ -n "$ora_ignore_addons" ] ; then
 		ora_ignore_addons="${ora_ignore_addons})"
 	fi
